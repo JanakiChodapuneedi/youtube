9c0ac95f42dc4b82c59aaceadd007c92c4f18d6e
JanakiChodapuneedi <janakichodapuneedi@gmail.com>
2023-02-15 00:16:44 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">
+       
+          Youtube
+        
+      </h1>
+    </div>
+  );
+}
+
+export default App;
