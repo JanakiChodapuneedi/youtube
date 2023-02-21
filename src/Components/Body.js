162d914ffb96f415939462ca14b55fdd74977eab
JanakiChodapuneedi <janakichodapuneedi@gmail.com>
2023-02-21 13:25:10 +0530

    updated for thumbnail

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import MainContainer from './MainContainer';
 import SideBar from './SideBar';
-import VideoCard from './VideoCard'
 
 const Body = () => {
   return (
--- src/Components/VideoCard.js
+++ src/Components/VideoCard.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "../logo.png";
 
 const VideoCard = ({ info }) => {
   console.log(info);
@@ -7,7 +6,7 @@ const VideoCard = ({ info }) => {
   const { channelTitle, title, thumbnails } = snippet;
   return (
     <div className="p-2 m-2 w-72 shadow-md">
-      <img className="rounded-lg" src={thumbnails.medium.url} />
+      <img className="rounded-lg" alt="thumbnail" src={thumbnails.medium.url} />
       <div className="font-bold">{channelTitle}</div>
       <div>{title}</div>
       <div>{statistics.viewCount} views </div>
